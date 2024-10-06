9531c48f4d0cb7febe626e2544c524467b4c2148
es-pesk <168341077+es-pesk@users.noreply.github.com>
2024-10-06 13:45:30 +0300

    dl_2_hw.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -3,10 +3,17 @@ import torch
 from torch import nn
 
 def create_model():
+    NN = nn.Sequential(nn.Linear(784,256 , bias=True),
+                   nn.ReLU(),
+                   nn.Linear(256, 16, bias=True),
+                   nn.ReLU(),
+                   nn.Linear(16, 10, bias=True))    # Linear layer mapping from 784 features, so it should be 784->256->16->10
+
     # your code here
+
     # return model instance (None is just a placeholder)
 
-    return None
+    return NN
 
 def count_parameters(model):
     # your code here
